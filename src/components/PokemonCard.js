52edd73fa9b9ceb08c5303f79d435d9cda989c7a
sedat <sedatcyalcin@gmail.com>
2020-04-24 14:04:26 +0300

    Migrate from react-pokemon-sprites to img

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -4,68 +4,75 @@ import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import PokeSprite from "react-poke-sprites";
 import { NavLink } from "react-router-dom";
 import { getPokemon } from "../api/client";
+import PropTypes from "prop-types";
 
 const useStyles = makeStyles({
-    root: {
-        maxWidth: 300,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        margin: "10px"
-    },
+  root: {
+    maxWidth: 300,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: "10px",
+  },
 
-    sprite: {
-        width: "70px",
-        display: "block",
-        marginTop: "15px",
-        marginLeft: "auto",
-        marginRight: "auto",
-        "& img": {
-            height: "70px"
-        }
-    }
+  sprite: {
+    width: "70px",
+    display: "block",
+    marginTop: "15px",
+    marginLeft: "auto",
+    marginRight: "auto",
+    "& img": {
+      height: "70px",
+    },
+  },
 });
 function PokemonCard({ name }) {
-    const classes = useStyles();
-    const [pokemonData, setPokemonData] = useState(null);
+  const classes = useStyles();
+  const [pokemonData, setPokemonData] = useState(null);
 
-    useEffect(() => {
-        getPokemon(`/pokemon/${name}`).then(res => {
-            setPokemonData(res.data);
-        });
-    }, [name]);
+  useEffect(() => {
+    getPokemon(`/pokemon/${name}`).then((res) => {
+      setPokemonData(res.data);
+    });
+  }, [name]);
 
-    return pokemonData ? (
-        <Card className={classes.root}>
-            <NavLink
-                style={{ width: "100%" }}
-                to={{
-                    pathname: `/pokemon/${name}`,
-                    state: { pokemon: pokemonData }
-                }}
+  return pokemonData ? (
+    <Card className={classes.root}>
+      <NavLink
+        style={{ width: "100%" }}
+        to={{
+          pathname: `/pokemon/${name}`,
+          state: { pokemon: pokemonData },
+        }}
+      >
+        <CardActionArea>
+          <div className={classes.sprite}>
+            <img
+              src={`http://img.pokemondb.net/artwork/${name}.jpg`}
+              alt={name}
+            />
+          </div>
+
+          <CardContent>
+            <Typography
+              style={{ textAlign: "center" }}
+              gutterBottom
+              variant="h5"
+              component="h2"
             >
-                <CardActionArea>
-                    <div className={classes.sprite}>
-                        <PokeSprite pokemon={name} />
-                    </div>
-
-                    <CardContent>
-                        <Typography
-                            style={{ textAlign: "center" }}
-                            gutterBottom
-                            variant="h5"
-                            component="h2"
-                        >
-                            {name}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-            </NavLink>
-        </Card>
-    ) : null;
+              {name}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </NavLink>
+    </Card>
+  ) : null;
 }
+
+PokemonCard.propTypes = {
+  name: PropTypes.string,
+};
 
 export default PokemonCard;
