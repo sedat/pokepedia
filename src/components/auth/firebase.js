b847bc87899294366fb2634a72a1455e3afcf23f
sedat <sedatcyalcin@gmail.com>
2020-04-24 14:34:49 +0300

    Hide firebase api key

--- src/components/auth/firebase.js
+++ src/components/auth/firebase.js
@@ -1,7 +1,8 @@
 import * as firebase from "firebase";
 import "firebase/firestore";
 import "firebase/auth";
-const firebaseConfig = {};
+import config from "../../config";
+const firebaseConfig = config.API_KEY;
 // Initialize Firebase
 
 export default firebase.initializeApp(firebaseConfig);
